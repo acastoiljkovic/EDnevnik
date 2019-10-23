1b5901ad7eeb79e482da8568761a643131ceeb86
acastoiljkovic <aca.stoiljkovic@elfak.rs>
2019-10-23 15:37:46 +0200

    Added App
    There is desktop and web applications for E-Dnevnik

---
+++ EDnevnikWebApp/Controllers/PredmetController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EDnevnikMapiranje;
+using EDnevnikMapiranje.Entiteti;
+using EDnevnikMapiranje.DTOs;
+
+namespace EDnevnikWebApp.Controllers
+{
+    public class PredmetController : ApiController
+    {
+        public IEnumerable<PredmetView> Get()
+        {
+            DataProvider data = new DataProvider();
+            return data.GetPredmetViews();
+        }
+
+        public PredmetView Get(int id)
+        {
+            DataProvider data = new DataProvider();
+            return data.GetPredmetViewId(id);
+        }
+
+        public int Put(int id, [FromBody]Predmet f)
+        {
+            DataProvider provider = new DataProvider();
+            return provider.UpdatePredmet(id, f);
+        }
+
+        public int Post([FromBody]Predmet f)
+        {
+            DataProvider provider = new DataProvider();
+            return provider.AddPredmet(f);
+        }
+
+        public int Delete(int id)
+        {
+            DataProvider provider = new DataProvider();
+            return provider.RemovePredmet(id);
+        }
+
+    }
+}
